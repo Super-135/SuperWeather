47da8c8bd844740b86ba7477f78186b40a31be3b
Super135 <super05061982@gmail.com>
2020-10-16 14:41:22 +0300

    Version_0_4

--- app/src/main/java/com/superlip/superweather/CitySelectionScreen.java
+++ app/src/main/java/com/superlip/superweather/CitySelectionScreen.java
@@ -1,15 +1,25 @@
 package com.superlip.superweather;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 public class CitySelectionScreen extends AppCompatActivity {
     private ImageView iViewCurrent;
     private ImageView iViewFavourites;
+    private TextView tViewCurrentCity;
+    private TextView tViewtFavouritesCity;
+    final static String currentPointKey1 = "currentPointKey1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +27,25 @@ public class CitySelectionScreen extends AppCompatActivity {
         setContentView(R.layout.activity_city_selection_screen);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         findView();
+        showBackBtn();
         setOniViewCurrent();
+        showDataFromFirstActivity();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()== android.R.id.home) {
+            Intent intentCity = new Intent();
+            String strDate = tViewtFavouritesCity.getText().toString();
+            if (! strDate.equals("")) {
+                intentCity.putExtra(currentPointKey1, strDate);
+            } else {
+                intentCity.putExtra(currentPointKey1, tViewCurrentCity.getText().toString());
+            }
+            setResult(RESULT_OK, intentCity);
+            finish();
+        }
+        return true;
     }
 
     private void setOniViewCurrent() {
@@ -31,6 +59,15 @@ public class CitySelectionScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),getString(R.string.favourites),Toast.LENGTH_SHORT).show();
+                Intent intentCity = new Intent();
+                String strDate = tViewtFavouritesCity.getText().toString();
+                if (! strDate.equals("")) {
+                    intentCity.putExtra(currentPointKey1, strDate);
+                } else {
+                    intentCity.putExtra(currentPointKey1, tViewCurrentCity.getText().toString());
+                }
+                setResult(RESULT_OK, intentCity);
+                finish();
             }
         });
     }
@@ -38,5 +75,19 @@ public class CitySelectionScreen extends AppCompatActivity {
     private void findView() {
         iViewCurrent = findViewById(R.id.iViewCurrent);
         iViewFavourites = findViewById(R.id.iViewFavourites);
+        tViewCurrentCity = findViewById(R.id.tViewCurrentCity);
+        tViewtFavouritesCity = findViewById(R.id.tViewtFavouritesCity);
+    }
+
+    private void showBackBtn(){
+        try {
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void showDataFromFirstActivity(){
+        tViewCurrentCity.setText(getIntent().getStringExtra(MainActivity.currentPointKey));
     }
 }
